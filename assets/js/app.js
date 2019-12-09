339f4a8f1be9c24665014e903afaa0402473b8b2
Janine White <janine.white@gmail.com>
2019-12-08 21:59:34 -0700

    Choose variables and plot working

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,26 +1,47 @@
+//Text
+function capitalizeFirstLetter(text) {
+    return text[0].toUpperCase() + text.slice(1).toLowerCase();
+}
+function titleCase(text) {
+    console.log(text);
+    text_split = text.toLowerCase().split(" ");
+    return text_split.map(x => capitalizeFirstLetter(x)).join(" ");
+}
+
 var dataFile = "https://raw.githubusercontent.com/janinewhite/D3-challenge/master/assets/db/data.csv"
 var csvData;
+var xAxis = "age";
+var yAxis = "income";
+var xVar, yVar;
+
+var selectX = d3.select(".select-x");
+selectX.on("change", function(){
+    xAxis = d3.event.target.value;
+    console.log("X Axis: "+xAxis);
+});
+var selectY = d3.select(".select-y");
+selectY.on("change", function(){
+    yAxis = d3.event.target.value;
+    console.log("Y Axis: "+yAxis);
+});
+var graphButton = d3.select(".graph-button");
+
 var svgWidth = 400;
 var svgHeight = 300;
-
 var margin = {
   top: 10,
   right: 40,
   bottom: 60,
-  left: 75
+  left: 60
 };
-
 var width = svgWidth - margin.left - margin.right;
 var height = svgHeight - margin.top - margin.bottom;
 
-// Create an SVG wrapper, append an SVG group that will hold our chart, and shift the latter by left and top margins.
+// Create an SVG wrapper
 var svg = d3.select(".chart")
   .append("svg")
   .attr("width", svgWidth)
   .attr("height", svgHeight);
-
-var chartGroup = svg.append("g")
-  .attr("transform", `translate(${margin.left}, ${margin.top})`);
 
 d3.csv(dataFile).then(data => {
     csvData = data;
@@ -32,79 +53,89 @@ d3.csv(dataFile).then(data => {
         data.obesity = +data.obesity;
         data.smokes = +data.smokes;
     });
-    
-    let xAxis = "age";
-    let yAxis = "income";
-    let xVar, yVar;
 
-    // Create axes
-    let xLinearScale = d3.scaleLinear()
-        .domain([20, d3.max(csvData, d => {
-            eval("xVar = d."+xAxis+";")
-            return xVar;
-        })])
-        .range([0, width]);
-    let yLinearScale = d3.scaleLinear()
-        .domain([0, d3.max(csvData, d => {
-            eval("yVar = d."+yAxis+";")
-            return yVar;
-        })])
-        .range([height, 0]);
-    let bottomAxis = d3.axisBottom(xLinearScale);
-    let leftAxis = d3.axisLeft(yLinearScale);
-    chartGroup.append("g")
-        .attr("transform", `translate(0, ${height})`)
-        .call(bottomAxis);
-    chartGroup.append("g")
-        .call(leftAxis);
+    graphButton.on("click",function(){
+        console.log("Graphing "+xAxis+" vs "+yAxis)
+        // Clear svg
+        svg.selectAll("*").remove();
+        // Append svg group to hold chart
+        var chartGroup = svg.append("g")
+            .attr("transform", `translate(${margin.left}, ${margin.top})`);
+        // Set chart title
+        d3.select(".chart-title").text("US States: "+titleCase(xAxis)+" vs "+titleCase(yAxis))
+        // Create axes
+        let xLinearScale = d3.scaleLinear()
+            .domain([
+                d3.min(csvData, d => {
+                    eval("xVar = d."+xAxis+";")
+                    return xVar;
+                })*0.8, 
+                d3.max(csvData, d => {
+                    eval("xVar = d."+xAxis+";")
+                    return xVar;
+                })*1.1
+            ])
+            .range([0, width]);
+        let yLinearScale = d3.scaleLinear()
+            .domain([
+                d3.min(csvData, d => {
+                    eval("yVar = d."+yAxis+";")
+                    return yVar;
+                })*0.8,
+                d3.max(csvData, d => {
+                    eval("yVar = d."+yAxis+";")
+                    return yVar;
+                })*1.1
+            ])
+            .range([height, 0]);
+        let bottomAxis = d3.axisBottom(xLinearScale);
+        let leftAxis = d3.axisLeft(yLinearScale);
+        chartGroup.append("g")
+            .attr("transform", `translate(0, ${height})`)
+            .call(bottomAxis);
+        chartGroup.append("g")
+            .call(leftAxis);
     
-    // Label axes
-    chartGroup.append("text")
-      .attr("transform", "rotate(-90)")
-      .attr("y", 0 - margin.left)
-      .attr("x", 0 - (height / 2))
-      .attr("dy", "1em")
-      .attr("class", "axisText")
-      .text(yAxis.toUpperCase());
-    chartGroup.append("text")
-      .attr("transform", `translate(${width / 2}, ${height + margin.top + 30})`)
-      .attr("class", "axisText")
-      .text(xAxis.toUpperCase());
-
-    // Create circles
-    let circlesGroup = chartGroup.selectAll("circle")
-        .data(csvData)
-        .enter()
-        .append("circle")
-        .attr("cx", d => {            
-            eval("xVar = d."+xAxis+";");
-            return xLinearScale(xVar);
-        })
-        .attr("cy", d => {            
-            eval("yVar = d."+yAxis+";");
-            return yLinearScale(yVar);
-        })
-        .attr("r", "3")
-        .attr("fill", "blue")
-        .attr("opacity", ".5");
+        // Label axes
+        chartGroup.append("text")
+            .attr("transform", "rotate(-90)")
+            .attr("y", 0 - margin.left)
+            .attr("x", 0 - (height / 2))
+            .attr("dy", "1em")
+            .attr("class", "axisText")
+            .text(titleCase(yAxis));
+        chartGroup.append("text")
+            .attr("transform", `translate(${width / 2}, ${height + margin.top + 30})`)
+            .attr("class", "axisText")
+            .text(titleCase(xAxis));
     
         // initialize tooltips
-    let toolTip = d3.tip()
-      .attr("class", "tooltip")
-      .offset([80, -60])
-      .html(function(d) {return (`State: ${d.abbr}<br>${xAxis}: ${xVar}<br>${yAxis}:${yVar}`);
-      });
-    
-    // create tooltip
-    chartGroup.call(toolTip);
-    
-    // create event listeners for tooltip
-    circlesGroup
-        .on("click", function(data) {
-            toolTip.show(data, this);
-        })
-        .on("mouseout", function(data, index) {
-            toolTip.hide(data);
-        });
-    
+        let toolTip = d3.tip()
+            .attr("class", "tooltip")
+            .offset([80, -60])
+            .html(function(d) {
+                return (`State: ${(d.abbr).toUpperCase()}<br>${titleCase(xAxis)}: ${xVar}<br>${titleCase(yAxis)}: ${yVar}`);
+            });
+        // create tooltip
+        svg.call(toolTip);
+
+        // Create circles
+        let circlesGroup = chartGroup.selectAll("circle")
+            .data(csvData)
+            .enter()
+            .append("circle")
+            .attr("cx", d => {            
+                eval("xVar = d."+xAxis+";");
+                return xLinearScale(xVar);
+            })
+            .attr("cy", d => {            
+                eval("yVar = d."+yAxis+";");
+                return yLinearScale(yVar);
+            })
+            .attr("r", "3")
+            .attr("fill", "blue")
+            .attr("opacity", ".5")
+            .on('mouseover',toolTip.show)
+            .on('mouseout',toolTip.hide);
+    });
 }).catch(error => console.log(error));

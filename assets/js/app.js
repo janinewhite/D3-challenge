edc777ddadda76418674ea60f7cbfb8c7cdc939b
Janine White <janine.white@gmail.com>
2019-12-12 00:49:25 -0700

    Added logging and fixed selection value

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,4 @@
-//Text
+// Text control
 function capitalizeFirstLetter(text) {
     return text[0].toUpperCase() + text.slice(1).toLowerCase();
 }
@@ -7,32 +7,37 @@ function titleCase(text) {
     return text_split.map(x => capitalizeFirstLetter(x)).join(" ");
 }
 
+// Initialize variables
 var dataFile = "https://raw.githubusercontent.com/janinewhite/D3-challenge/master/assets/db/data.csv"
 var csvData;
 var xAxis = "age";
 var yAxis = "income";
 var xVar, yVar;
+var clicks = 0;
 
+// Create selection event handlers
 var selectX = d3.select(".select-x");
 selectX.on("change", function(){
     xAxis = d3.event.target.value;
-    console.log("X Axis: "+xAxis);
+    console.log("X axis selected: "+xAxis);
 });
 var selectY = d3.select(".select-y");
 selectY.on("change", function(){
     yAxis = d3.event.target.value;
-    console.log("Y Axis: "+yAxis);
+    console.log("Y axis selected: "+yAxis);
 });
-var graphButton = d3.select(".graph-button");
 
-var svgWidth = 400;
-var svgHeight = 300;
+// Initialize svg
 var margin = {
   top: 10,
   right: 40,
   bottom: 60,
   left: 60
 };
+//var svgWidth = +d3.select('.chart').style('width').slice(0, -2) - margin.left - margin.right;
+var svgWidth = 400;
+var svgHeight = svgWidth - ((margin.left + margin.right) - (margin.top + margin.bottom));
+
 var width = svgWidth - margin.left - margin.right;
 var height = svgHeight - margin.top - margin.bottom;
 
@@ -42,48 +47,61 @@ var svg = d3.select(".chart")
   .attr("width", svgWidth)
   .attr("height", svgHeight);
 
+// Get Data
 d3.csv(dataFile).then(data => {
     csvData = data;
     csvData.forEach(data => {
-        data.income = +data.income;
-        data.age = +data.age;
-        data.poverty = +data.poverty;
-        data.healthcare = +data.healthcare;
-        data.obesity = +data.obesity;
-        data.smokes = +data.smokes;
+        csvData.income = +data.income;
+        csvData.age = +data.age;
+        csvData.poverty = +data.poverty;
+        csvData.healthcare = +data.healthcare;
+        csvData.obesity = +data.obesity;
+        csvData.smokes = +data.smokes;
     });
-
+    
+    // Button event handler
+    var graphButton = d3.select(".graph-button");
     graphButton.on("click",function(){
+        console.log("Number of button clicks: "+clicks)
+        clicks += 1
         console.log("Graphing "+xAxis+" vs "+yAxis)
+        // Set chart title
+        d3.select(".chart-title").text("US States: "+titleCase(xAxis)+" vs "+titleCase(yAxis))
         // Clear svg
         svg.selectAll("*").remove();
         // Append svg group to hold chart
         var chartGroup = svg.append("g")
             .attr("transform", `translate(${margin.left}, ${margin.top})`);
-        // Set chart title
-        d3.select(".chart-title").text("US States: "+titleCase(xAxis)+" vs "+titleCase(yAxis))
         // Create axes
         let xLinearScale = d3.scaleLinear()
             .domain([
                 d3.min(csvData, d => {
-                    eval("xVar = d."+xAxis+";")
-                    return xVar;
+                    let xVarMin;
+                    eval("xVarMin = d."+xAxis+";");
+                    console.log("Scale "+d.abbr+" "+xAxis+" min: "+xVarMin);
+                    return xVarMin;
                 })*0.95, 
                 d3.max(csvData, d => {
-                    eval("xVar = d."+xAxis+";")
-                    return xVar;
+                    let xVarMax;
+                    eval("xVarMax = d."+xAxis+";");
+                    console.log("Scale "+d.abbr+" "+xAxis+" max: "+xVarMax);
+                    return xVarMax;
                 })
             ])
             .range([0, width]);
         let yLinearScale = d3.scaleLinear()
             .domain([
                 d3.min(csvData, d => {
-                    eval("yVar = d."+yAxis+";")
-                    return yVar;
+                    let yVarMin, yState;
+                    eval("yVarMin = d."+yAxis.toLowerCase()+";");
+                    console.log("Scale "+d.abbr+" "+yAxis+" min: "+yVarMin);
+                    return yVarMin;
                 })*0.95,
                 d3.max(csvData, d => {
-                    eval("yVar = d."+yAxis+";")
-                    return yVar;
+                    let yVarMax, yState;
+                    eval("yVarMax = d."+yAxis.toLowerCase()+";")
+                    console.log("Scale "+d.abbr+" "+yAxis+" max: "+yVarMax);
+                    return yVarMax;
                 })
             ])
             .range([height, 0]);
@@ -113,7 +131,14 @@ d3.csv(dataFile).then(data => {
             .attr("class", "tooltip")
             .offset([80, -60])
             .html(function(d) {
-                return (`State: ${(d.abbr).toUpperCase()}<br>${titleCase(xAxis)}: ${xVar}<br>${titleCase(yAxis)}: ${yVar}`);
+                let state = d.state;
+                let state_abbr = d.abbr.toUpperCase();
+                let first_data = eval("d."+xAxis);
+                let second_data = eval("d."+yAxis);
+                let first_axis = titleCase(xAxis);                
+                let second_axis = titleCase(yAxis);
+                console.log("Tip - "+d.abbr+" "+xAxis+": "+first_data+", "+yAxis+": "+second_data);
+                return (`State: ${state} (${state_abbr})<br/>${first_axis}: ${first_data}<br/>${second_axis}: ${second_data}`);
             });
         // create tooltip
         svg.call(toolTip);
@@ -123,13 +148,19 @@ d3.csv(dataFile).then(data => {
             .data(csvData)
             .enter()
             .append("circle")
-            .attr("cx", d => {            
-                eval("xVar = d."+xAxis+";");
-                return xLinearScale(xVar);
+            .attr("cx", d => {
+                let circleX;
+                eval("circleX = d."+xAxis+";");
+                let circleXtrans = xLinearScale(circleX);
+                console.log("Circle X - "+d.abbr+" "+xAxis+": "+circleX+", scaled "+circleXtrans);
+                return circleXtrans;
             })
-            .attr("cy", d => {            
-                eval("yVar = d."+yAxis+";");
-                return yLinearScale(yVar);
+            .attr("cy", d => {
+                let circleY;
+                eval("circleY = d."+yAxis+";");
+                let circleYtrans = yLinearScale(circleY);
+                console.log("Circle Y - "+d.abbr+" "+xAxis+": "+circleY+", scaled "+circleYtrans);
+                return circleYtrans;
             })
             .attr("r", "10")
             .attr("fill", "blue")
@@ -142,16 +173,30 @@ d3.csv(dataFile).then(data => {
             .append("text")
             .text(d => d.abbr)
             .attr("class","text-in-circle")
-            .attr("dx", d => {            
-                        eval("xVar = d."+xAxis+";");
-                        return xLinearScale(xVar)-6;
+            .attr("dx", d => {
+                let xVarText;
+                eval("xVarText = d."+xAxis+";");
+                let textX = xLinearScale(xVarText)-6;
+                console.log("Text X - "+d.abbr+" "+xAxis+": "+xVarText+", scaled "+textX);
+                return textX;
             })
-            .attr("dy", d => {            
-                        eval("yVar = d."+yAxis+";");
-                        return yLinearScale(yVar)+4;
+            .attr("dy", d => {
+                let yVarText;
+                eval("yVarText = d."+yAxis+";");
+                let textY = yLinearScale(yVarText)+4;
+                console.log("Text Y - "+d.abbr+" "+yAxis+": "+yVarText+", scaled "+textY);
+                return textY;
             })
-            .attr("font-size", 8)
-            .attr("font-weight", "bold")
-            .attr("opacity", ".75");
-    }); 
+            .style("font-size", 8)
+            .style("font-weight", "bold")
+            .style("fill","white")
+            ;
+    });
+    
+    
+    // If button hasn't been clicked, send click event to populate chart using default selections.
+    if (clicks == 0) {
+        document.getElementById('graph-button').click();
+    }
 }).catch(error => console.log(error));
+
